88bf8fdbd6c39582792bd6f0a45bc4078823bcf1
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-03-25 16:30:37 +0545

    profile for company

--- js/profile.js
+++ js/profile.js
@@ -3,17 +3,27 @@ var domain = "https://fourspace.herokuapp.com";
 function show_details(user) {
     var name = user.name;
     var email = user.email;
-    var rating = user.rating;
-    var stars = "⭐";
-    for (var i = 0; i < Math.ceil(user.rating / 500); ++i) {
-        stars += "⭐";
-    }
-    var available = user.available ? "YES" : "NO";
+
     document.getElementById("name").innerHTML = "Name: " + name;
     document.getElementById("email").innerHTML = "Email: " + email;
-    document.getElementById("ratings").innerHTML = "Rating: " + rating;
-    document.getElementById("stars").innerHTML = "Stars: " + stars;
-    document.getElementById("available").innerHTML = "Available: " + available;
+
+    if (user.type == "user") {
+        var rating = user.rating;
+        var stars = "⭐";
+        for (var i = 0; i < Math.ceil(user.rating / 500); ++i) {
+            stars += "⭐";
+        }
+        var available = user.available ? "YES" : "NO";
+        document.getElementById("ratings").innerHTML = "Rating: " + rating;
+        document.getElementById("stars").innerHTML = "Stars: " + stars;
+        document.getElementById("available").innerHTML = "Available for hire: " + available;
+    } else {
+        var openings = user.openings;
+        var stars = "⭐⭐⭐⭐⭐";
+
+        document.getElementById("ratings").innerHTML = "Openings: " + openings;
+        document.getElementById("stars").innerHTML = "Stars: " + stars;
+    }
 }
 
 window.onload = function () {
