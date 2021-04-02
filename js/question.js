e40d12d2754368ff0a60223fea57b68b140d8d1a
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-04-02 18:41:32 +0545

    running the code and recieve output

--- js/question.js
+++ js/question.js
@@ -1,5 +1,8 @@
 const editorDiv = document.getElementById("editor");
 const langSelect = document.getElementById("lang");
+const inputField = document.getElementById("input");
+const outputField = document.getElementById("output");
+const url = "http://localhost:8080";
 
 var editor = CodeMirror(editorDiv, {
     lineNumbers: true,
@@ -9,10 +12,54 @@ var editor = CodeMirror(editorDiv, {
     viewportMargin: 2,
     lint: true,
     closeBrackets: true,
+    value: "",
 });
 
-const langChange = () => {
+langSelect.onchange = () => {
     editor.setOption("mode", langSelect.value);
 };
 
-langSelect.onchange = langChange;
+const langMap = {
+    python: "py",
+    clike: "cpp",
+    java: "java",
+};
+
+async function run() {
+    try {
+        outputField.value = "Running ...";
+        var code = editor.getValue();
+        var language = langSelect.value;
+        var input = inputField.value;
+        var token = localStorage.getItem("token");
+        if (!token) {
+            location.href = "/login.html";
+        }
+
+        var response = await fetch(url + "/run", {
+            method: "POST",
+            // mode: "no-cors",
+            headers: {
+                "Content-type": "application/json",
+            },
+
+            body: JSON.stringify({
+                token,
+                code,
+                language: langMap[language],
+                input,
+            }),
+        });
+        var output = await response.json();
+        if (output.status) {
+            outputField.style.outline = "2px solid green";
+        } else {
+            outputField.value = output.message;
+            outputField.style.outline = "2px solid green";
+        }
+        outputField.value = output.output;
+    } catch (e) {
+        outputField.value = "";
+        console.log(e);
+    }
+}
