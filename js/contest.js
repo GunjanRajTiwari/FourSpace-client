f9f52effe98164bef769eb59e9b0bca4d26f6de7
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-05-23 13:21:14 +0545

    Contestwise Leaderboard Added

--- js/contest.js
+++ js/contest.js
@@ -2,9 +2,10 @@ const title = document.getElementById("title");
 const company = document.getElementById("company");
 const info = document.getElementById("info");
 const questions = document.getElementById('questions');
+const leaderboard = document.getElementById('leaderboard');
 
-function solve(qid){
-    location.href = '/question.html?qid='+qid;
+function solve(qid) {
+    location.href = '/question.html?qid=' + qid;
 }
 
 window.onload = () => {
@@ -13,6 +14,9 @@ window.onload = () => {
     if (!cid) {
         location.href = "/contests.html";
     } else {
+        leaderboard.addEventListener('click', () => {
+            location.href = '/leaderboard.html?cid=' + cid;
+        })
         fetch(`${domain}/contests/${cid}`, {
                 headers: {
                     "Content-type": "application/json",
@@ -25,14 +29,14 @@ window.onload = () => {
                 company.innerHTML = result.company_email;
                 info.innerHTML = result.info;
 
-                questions.innerHTML='';
-                if(result.questionCount===0){
+                questions.innerHTML = '';
+                if (result.questionCount === 0) {
                     questions.innerHTML = "No questions added 🙁"
                 } else {
                     result.questions.forEach((problem, index) => {
                         const question = document.createElement('div');
                         question.classList.add('question');
-                        question.innerHTML=`
+                        question.innerHTML = `
                         <span>
                         #${index+1} ${problem.title}
                         </span>
